d176c92e834657a1951e7e64f7cdb165cc5edf87
aman qureshi <>
2025-02-12 05:45:10 -0800

    Initial-Stages

--- Backend-folder/app.js
+++ Backend-folder/app.js
@@ -9,7 +9,6 @@ const connectDB = require('./DB/db');
 const userRoutes = require('./Routes/userRoutes');
 
 
-
 connectDB();    // Connect to MongoDB
 app.use(cors({
   origin: ['https://webflix-app-pr72.onrender.com'],
@@ -21,18 +20,26 @@ app.use(express.json()); // Parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 app.use(cookieParser());    //cookie parser
 
+
 // Static folder for serving uploaded images
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+// Serve static files from React app in production
+if (process.env.NODE_ENV === 'production') {
+  // Serve static files from React app
+  app.use(express.static(path.join(__dirname, 'Frontend-folder/build')));
 
+  // Catch-all route to handle React routing
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Frontend-folder/build', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('hello from serverside!');
+  });
+}
 
 //Server Routes-
-app.get('/', (req, res) => {
-    res.send('hello from serverside!');
-});
-
 app.use('/users', userRoutes);
-
-
 
 module.exports = app;
