a0fe5cba66cbbce23f68dd49213f3ecf1fa14cea
aman qureshi <>
2025-02-13 00:53:42 -0800

    Initial-Stages

--- Frontend-folder/src/main.jsx
+++ Frontend-folder/src/main.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { store } from './Store/Store.jsx'
-import UserContext from './Context/UserContext.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { HashRouter } from "react-router-dom"; // BrowserRouter ki jagah HashRouter use kiya
+import { Provider } from "react-redux";
+import { store } from "./Store/Store.jsx";
+import UserContext from "./Context/UserContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-<Provider store={store}>
- <UserContext>
-  <BrowserRouter>
-   <App />
-  </BrowserRouter>
- </UserContext>
-</Provider>
-)
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <UserContext>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </UserContext>
+  </Provider>
+);
