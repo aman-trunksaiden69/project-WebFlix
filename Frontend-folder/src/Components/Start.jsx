e0f8d106e33ebda1e5b0e162fa10a1b6f38aa6be
aman qureshi <>
2025-02-17 23:23:01 -0800

    Initial-Stages

--- Frontend-folder/src/Components/Start.jsx
+++ Frontend-folder/src/Components/Start.jsx
@@ -9,7 +9,7 @@ const Start = () => {
   return (
     <div className='flex flex-col justify-between h-screen w-screen bg-[#000] text-white font-["gilroy"]'>
     
-    <div className='h-[60%] w-full'>
+    <div className='h-[70%] w-full'>
      <img className='h-full w-full object-cover' src="/webflixstart.jpg" alt="" />  
 
      <div className='bg-[#00000071] flex flex-col gap-2 items-center justify-center w-full h-[20%] absolute top-[46%]'>
@@ -19,10 +19,10 @@ const Start = () => {
 
     </div>
 
-    <div className='h-[40%] flex flex-col items-center justify-between'>
+    <div className='h-[30%] flex flex-col items-center justify-between'>
     <p className='text-center ml-1 leading-4 text-sm'>Watch unlimited movies, series & TV shows anywhere, anytime</p>  
     
-    <div className='w-full border-t-[1px] rounded-t-xl border-pink-400 flex items-center justify-center'>
+    <div className='w-full p-3 border-t-[1px] rounded-t-xl border-pink-400 flex items-center justify-center'>
 
       <Link to="/Login" 
         className='px-20 py-3 text-xl flex gap-2 bg-blue-600 rounded-md hover:border-[1px] hover:text-black hover:border-white hover:bg-blue-300'>
