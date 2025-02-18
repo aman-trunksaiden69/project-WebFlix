c1dc08e743239c5dd357cdeb4c998aa13b9c3205
aman qureshi <>
2025-02-17 22:17:47 -0800

    Initial-Stages

--- Frontend-folder/src/Components/Login.jsx
+++ Frontend-folder/src/Components/Login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
     try {
       const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/users/login`, newUser, {
         headers: {
-          "Content-Type": "application/json"  // Ensures JSON format
+          "Content-Type": "application/json"  //JSON format
         }
       });
 
@@ -56,17 +56,18 @@ const Login = () => {
         }}
         className="w-screen h-screen flex items-center justify-center bg-cover bg-center font-['gilroy']"
       >
-        <div className="w-[90%] h-[75vh] shadow-[2px_4px_4px_rgba(0,1,0,0.5)] bg-[#000000b8] border-[1px] border-white rounded-xl flex flex-col items-center justify-center">
-          <h1 className="text-4xl font-bold text-white mb-8">Login</h1>
-          <div>
-            <form onSubmit={submitHandler} method="post" encType="multipart/form-data" className="flex flex-col items-center gap-3 justify-center">
+        <div className="w-[90%] h-[75vh] shadow-[2px_4px_4px_rgba(0,1,0,0.5)] bg-[#00000096] border-[1px] border-white rounded-xl flex flex-col items-center justify-center">
+          <h1 className="text-2xl font-bold text-white mb-8">Login</h1>
+          <div className='p-2'>
+            <form onSubmit={submitHandler} method="post" encType="multipart/form-data" 
+             className="flex flex-col items-center gap-3 justify-center">
               <input 
                 type="text" 
                 placeholder='Enter Email'
                 value={Email}
                 onChange={(e) => setEmail(e.target.value)}
                 required
-                className='focus:outline-none hover:border-pink-700 px-10 py-3 text-pink-400 font-medium rounded-sm bg-[#130F10] border-[2px] border-white placeholder-pink-400 placeholder:text-xl'
+                className='focus:outline-none hover:border-pink-700 px-6 py-2 text-pink-400 font-medium rounded-sm bg-[#130F10] border-[2px] border-white placeholder-pink-400 placeholder:text-md'
               />
 
               <input 
@@ -75,25 +76,25 @@ const Login = () => {
                 value={Password}
                 onChange={(e) => setPassword(e.target.value)}
                 required 
-                className='focus:outline-none hover:border-pink-700 px-10 py-3 text-pink-400 font-medium rounded-sm bg-[#130F10] border-[2px] border-white placeholder-pink-400 placeholder:text-xl'
+                className='focus:outline-none hover:border-pink-700 px-6 py-2 text-pink-400 font-medium rounded-sm bg-[#130F10] border-[2px] border-white placeholder-pink-400 placeholder:text-md'
               />
 
               {error && <div className="text-red-600 text-sm">{error}</div>}  {/* Display error */}
 
-              <div className='w-full'>
-                <button className="w-full flex justify-center py-2 rounded-md shadow-sm text-xl font-semibold text-white bg-blue-600 border-[1px] border-black hover:border-white hover:bg-blue-300 hover:text-black">
+              <div className='w-[100%]'>
+                <button className="w-full flex justify-center py-2 rounded-md shadow-sm text-md font-semibold text-white bg-blue-600 border-[1px] border-black hover:border-white hover:bg-blue-300 hover:text-black">
                   Login
                 </button>
               </div>
               <h1 className='text-zinc-400 text-md font-normal'>OR</h1>
-              <div className='w-full'>
-                <button className="w-full flex justify-center py-2 px-28 border border-transparent rounded-md shadow-sm text-xl font-semibold text-black bg-white focus:outline-none focus:ring-4 focus:ring-offset-2 focus:ring-indigo-500">
+              <div className='w-[100%]'>
+                <button className="w-full flex justify-center py-2 border border-transparent rounded-md shadow-sm text-xl font-semibold text-black bg-white focus:outline-none focus:ring-4 focus:ring-offset-2 focus:ring-indigo-500">
                   <FcGoogle />
                 </button>
               </div>
             </form>
             <div className='flex items-center justify-center mt-2'>
-              <p className="text-white font-medium">
+              <p className="text-white font-medium text-md">
                 New to WebFlix?<Link to="/Signup" className='hover:text-pink-400 pl-1 hover:underline'>Sign up</Link>
               </p>
             </div>
