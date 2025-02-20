a364caa098e167bc5cea0ecc4b2a2bab6631d6e1
aman qureshi <>
2025-02-20 00:44:49 -0800

    Initial-Stages

--- Frontend-folder/src/Components/Templates/Cardhorizon.jsx
+++ Frontend-folder/src/Components/Templates/Cardhorizon.jsx
@@ -4,9 +4,7 @@ import noimage from '/noimage.jpg'
 
 const Cardhorizon = ({data}) => {
   
-  return <div className='h-[50vh] font-["gilroy"] p-2 flex items-center'>
- 
-    <div className='cardhorizon w-[100%] flex h-[50vh] overflow-y-hidden border-[1px] border-pink-400 rounded-tl-3xl rounded-br-3xl'>
+  return <div className='cardhorizon w-[100%] flex h-[50vh] overflow-y-hidden border-[1px] border-pink-400 rounded-tl-3xl rounded-br-3xl'>
      
       {data.length > 0 ? data.map((d, i) => (
       <Link to={`/${d.media_type}/details/${d.id}`} key={i}
@@ -34,9 +32,8 @@ const Cardhorizon = ({data}) => {
         </h1>} 
 
           
-    </div>
-
   </div>
+
 }
 
 export default Cardhorizon
