ccd6c67d7c90f9d259cd9ff44dedbfa02a35ca1a
aman qureshi <>
2025-02-25 03:54:41 -0800

    Initial-Stages

--- Backend-folder/passport.js
+++ Backend-folder/passport.js
@@ -3,54 +3,61 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const userModel = require('./Models/userModel');
 require('dotenv').config();
 
-
 if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
   console.error("Missing Google OAuth Credentials in .env file!");
-};
+  process.exit(1); // Process exit if credentials are missing
+}
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'https://webflix-server-rcqi.onrender.com/auth/google/callback',
-    passReqToCallback: true
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: 'https://webflix-server-rcqi.onrender.com/auth/google/callback',
+      passReqToCallback: true,
     },
+    async (request, accessToken, refreshToken, profile, done) => {
+      try {
+        if (!profile) {
+          throw new Error("Google OAuth Profile Data is undefined!");
+        }
 
-    async ( accessToken, refreshToken, profile, done ) => {
+        console.log("Google OAuth Callback Triggered!");
+        console.log("Redirect URI Used:", 'https://webflix-server-rcqi.onrender.com/auth/google/callback');
+        console.log("Profile Data:", profile);
 
-    console.log("Google OAuth Callback Triggered!");
-    console.log("Redirect URI Used:", 'https://webflix-server-rcqi.onrender.com/auth/google/callback');
-    console.log("Profile Data:", profile);
+        let user = await userModel.findOne({ googleId: profile.id });
 
+        if (!user) {
+          user = new userModel({
+            googleId: profile.id,
+            name: profile.displayName || "No Name",
+            email: profile.emails?.[0]?.value || "No Email",
+            photo: profile.photos?.[0]?.value || "No Photo",
+          });
 
-        try {
-            let user = await userModel.findOne({ googleId: profile.id });
-      
-            if (!user) {
-              user = new userModel({
-                googleId: profile.id,
-                name: profile.displayName,
-                email: profile.emails[0]?.value || "",
-                photo: profile.photos[0]?.value || "",
-              });
-
-              await user.save();
-            }
-            done(null, user);
-          } catch (err) {
-            done(err, null);
-          }
+          await user.save();
+        }
+        return done(null, user);
+      } catch (err) {
+        console.error("Error in Google OAuth Strategy:", err);
+        return done(err, null);
+      }
     }
-
-));
+  )
+);
 
 passport.serializeUser((user, done) => {
-    done(null, user.id);
-});
-  
-passport.deserializeUser(async (id, done) => {
-    const user = await userModel.findById(id);
-    done(null, user);
+  done(null, user.id);
 });
 
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await userModel.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
 
 module.exports = passport;
