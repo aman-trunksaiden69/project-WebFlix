b5d8cf2cde32ea470b7b07bd98de6d30f9d951c5
aman qureshi <>
2025-03-06 03:08:48 -0800

    Initial-Stages

--- Frontend-folder/src/Components/Profile.jsx
+++ Frontend-folder/src/Components/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 import { userDataContext } from '../Context/UserContext';
 import axios from 'axios';
