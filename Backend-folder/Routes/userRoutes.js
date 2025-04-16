e97b35c0883731cb76548d8207d4b8fa0e51a421
aman qureshi <>
2025-04-16 01:46:09 -0700

    Initial-Stages

--- Backend-folder/Routes/userRoutes.js
+++ Backend-folder/Routes/userRoutes.js
@@ -79,8 +79,8 @@ router.get('/auth/google/callback', async (req, res) => {
       console.log(profileData.data);
       // Find or create user based on Google profile
       let user = await userModel.findOne({ googleId: profileData.data.sub });
-      
-      const token = user.generateAuthToken(); // Generate JWT token for the user
+
+      const token = await user.generateAuthToken(); // Generate JWT token for the user
 
       if (!user) {
         const profileImage = profileData.data.picture;
